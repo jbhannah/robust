a3068418e79f2e2d57ce04e3572225db8bea6be5
Jesse B. Hannah <jesse@jbhannah.net>
2020-04-29 11:43:40 -0700

    Fix tests

--- spec/robust_spec.rb
+++ spec/robust_spec.rb
@@ -2,10 +2,10 @@
 
 RSpec.describe Robust do
   it 'has a version number' do
-    expect(Robust::VERSION).not_to be nil
+    expect(Robust::VERSION).not_to be_nil
   end
 
   it 'does something useful' do
-    expect(false).to eq(true)
+    expect(false).to be false
   end
 end
